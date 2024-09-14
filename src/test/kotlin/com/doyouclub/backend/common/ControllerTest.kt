82a3e7e21f12e02b950aabf263716974dff102a9
earlgrey02 <san06036@naver.com>
2024-09-14 20:18:10 +0900

    Feat: 공통 컨트롤러 테스트 클래스 구현

---
+++ src/test/kotlin/com/doyouclub/backend/common/ControllerTest.kt
@@ -0,0 +1,30 @@
+package com.doyouclub.backend.common
+
+import com.doyouclub.backend.global.exception.GlobalExceptionHandler
+import io.kotest.core.spec.style.DescribeSpec
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Import
+import org.springframework.restdocs.RestDocumentationContextProvider
+import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@Import(GlobalExceptionHandler::class)
+@WebFluxTest
+@AutoConfigureRestDocs
+abstract class ControllerTest : DescribeSpec() {
+    @Autowired
+    private lateinit var applicationContext: ApplicationContext
+
+    @Autowired
+    private lateinit var restDocumentation: RestDocumentationContextProvider
+
+    protected val webClient: WebTestClient by lazy {
+        WebTestClient.bindToApplicationContext(applicationContext)
+            .configureClient()
+            .filter(WebTestClientRestDocumentation.documentationConfiguration(restDocumentation))
+            .build()
+    }
+}
