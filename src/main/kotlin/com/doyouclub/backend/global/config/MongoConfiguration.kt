182976200c8df4d35ec044f957eeb76367a21a40
earlgrey02 <san06036@naver.com>
2024-09-14 20:09:32 +0900

    Feat: MongoDB 설정 클래스 구현

---
+++ src/main/kotlin/com/doyouclub/backend/global/config/MongoConfiguration.kt
@@ -0,0 +1,8 @@
+package com.doyouclub.backend.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing
+
+@EnableReactiveMongoAuditing
+@Configuration
+class MongoConfiguration
