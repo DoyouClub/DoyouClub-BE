d96c7cb60abce4f32511fdc1ac7b756888507cfa
earlgrey02 <san06036@naver.com>
2024-09-14 20:01:29 +0900

    Feat: 전역 예외 처리를 수행하는 GlobalExceptionHandler 구현

---
+++ src/main/kotlin/com/doyouclub/backend/global/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,47 @@
+package com.doyouclub.backend.global.exception
+
+import com.doyouclub.backend.global.dto.ErrorResponse
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import kotlinx.coroutines.reactor.mono
+import mu.KotlinLogging
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.MediaType
+import org.springframework.http.server.reactive.ServerHttpResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.server.CoWebFilter
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+import kotlin.coroutines.CoroutineContext
+import kotlin.reflect.jvm.jvmName
+
+@Component
+class GlobalExceptionHandler(
+    private val objectMapper: ObjectMapper
+) : ErrorWebExceptionHandler {
+    private val logger = KotlinLogging.logger(this::class.jvmName)
+
+    override fun handle(exchange: ServerWebExchange, exception: Throwable): Mono<Void> =
+        mono(exchange.attributes[CoWebFilter.COROUTINE_CONTEXT_ATTRIBUTE] as CoroutineContext) {
+            with(exchange.response) {
+                val body = ErrorResponse(exception)
+
+                logger.error { "${exception::class.simpleName}(\"${exception.message}\") at ${exception.stackTrace[0]}" }
+
+                headers.contentType = MediaType.APPLICATION_JSON
+                statusCode = HttpStatusCode.valueOf(body.code)
+
+                writeBody(body)
+                    .awaitSingleOrNull()
+            }
+        }
+
+    private fun ServerHttpResponse.writeBody(body: Any): Mono<Void> =
+        writeWith(
+            Mono.just(
+                bufferFactory()
+                    .wrap(objectMapper.writeValueAsBytes(body))
+            )
+        )
+}
