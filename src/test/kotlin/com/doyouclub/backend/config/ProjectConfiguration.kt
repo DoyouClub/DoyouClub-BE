7bc6bd3bd53a20edcf8a9a7cf7f946aaa215746b
earlgrey02 <san06036@naver.com>
2024-09-14 20:18:00 +0900

    Feat: Kotest 설정 클래스 구현

---
+++ src/test/kotlin/com/doyouclub/backend/config/ProjectConfiguration.kt
@@ -0,0 +1,13 @@
+package com.doyouclub.backend.config
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.extensions.Extension
+import io.kotest.extensions.spring.SpringExtension
+import org.springframework.boot.test.context.TestConfiguration
+
+@TestConfiguration
+class ProjectConfiguration : AbstractProjectConfig() {
+    override val parallelism = 4
+
+    override fun extensions(): List<Extension> = listOf(SpringExtension)
+}
