cb287b5b9d638af7757c1b4cdbd891f9afeb3cb6
earlgrey02 <san06036@naver.com>
2024-09-14 20:09:01 +0900

    Feat: JwtAuthentication 구현

---
+++ src/main/kotlin/com/doyouclub/backend/global/jwt/security/JwtAuthentication.kt
@@ -0,0 +1,53 @@
+package com.doyouclub.backend.global.jwt.security
+
+import io.jsonwebtoken.Claims
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+
+data class JwtAuthentication(
+    val id: Int,
+    val roles: Set<GrantedAuthority>
+) : Authentication {
+    companion object {
+        operator fun invoke(claims: Claims): JwtAuthentication =
+            with(claims) {
+                JwtAuthentication(
+                    id = get("id") as Int,
+                    roles = (get("roles") as String)
+                        .split(",")
+                        .map(::SimpleGrantedAuthority)
+                        .toHashSet()
+                )
+            }
+    }
+
+    override fun getAuthorities(): Set<GrantedAuthority> = roles
+
+    override fun isAuthenticated(): Boolean = true
+
+    @Deprecated("JwtAuthentication does not include name.")
+    override fun getName(): String {
+        throw UnsupportedOperationException()
+    }
+
+    @Deprecated("JwtAuthentication does not support credential.")
+    override fun getCredentials() {
+        throw UnsupportedOperationException()
+    }
+
+    @Deprecated("JwtAuthentication does not support details.")
+    override fun getDetails() {
+        throw UnsupportedOperationException()
+    }
+
+    @Deprecated("JwtAuthentication does not support principal.")
+    override fun getPrincipal() {
+        throw UnsupportedOperationException()
+    }
+
+    @Deprecated("Cannot change the authenticated state of JwtAuthentication")
+    override fun setAuthenticated(isAuthenticated: Boolean) {
+        throw UnsupportedOperationException()
+    }
+}
