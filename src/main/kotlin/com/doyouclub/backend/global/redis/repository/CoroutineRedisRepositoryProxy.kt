658ec1eb79344d4b8362ccc8b9ac72d2f898974f
earlgrey02 <san06036@naver.com>
2024-09-25 15:57:29 +0900

    Feat: CoroutineRedisRepository의 프록시인 CoroutineRedisRepositoryProxy 구현

---
+++ src/main/kotlin/com/doyouclub/backend/global/redis/repository/CoroutineRedisRepositoryProxy.kt
@@ -0,0 +1,50 @@
+package com.doyouclub.backend.global.redis.repository
+
+import com.doyouclub.backend.global.redis.annotation.Key
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.repository.NoRepositoryBean
+import kotlin.time.Duration
+import kotlin.time.toJavaDuration
+
+@NoRepositoryBean
+class CoroutineRedisRepositoryProxy(
+    private val entityType: Class<*>,
+    private val redisTemplate: ReactiveRedisTemplate<String, String>,
+    private val objectMapper: ObjectMapper
+) : CoroutineRedisRepository<Any, String> {
+    override suspend fun findByKey(key: String): Any? =
+        redisTemplate.opsForValue()
+            .get(key.withId())
+            .map { objectMapper.readValue(it, entityType) }
+            .awaitSingleOrNull()
+
+    override suspend fun save(value: Any): Any =
+        redisTemplate.opsForValue()
+            .set(value.getKey(), objectMapper.writeValueAsString(value))
+            .thenReturn(value)
+            .awaitSingle()
+
+    override suspend fun save(value: Any, ttl: Duration): Any =
+        redisTemplate.opsForValue()
+            .set(value.getKey(), objectMapper.writeValueAsString(value), ttl.toJavaDuration())
+            .thenReturn(value)
+            .awaitSingle()
+
+    override suspend fun deleteByKey(key: String): Boolean =
+        redisTemplate.opsForValue()
+            .delete(key.withId())
+            .awaitSingle()
+
+    private fun Any.getKey() =
+        entityType.declaredFields
+            .first { it.isAnnotationPresent(Key::class.java) }
+            .apply { setAccessible(true) }
+            .get(this)
+            .toString()
+            .withId()
+
+    private fun String.withId(): String = "${entityType.simpleName}:$this"
+}
