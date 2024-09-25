b64423bb192296beb8b9667ea1579aff064a49c8
earlgrey02 <san06036@naver.com>
2024-09-25 12:55:15 +0900

    Feat: MappingMongoConverter가 _class 필드를 추가하지 않도록 설정

--- src/main/kotlin/com/doyouclub/backend/global/config/MongoConfiguration.kt
+++ src/main/kotlin/com/doyouclub/backend/global/config/MongoConfiguration.kt
@@ -1,8 +1,20 @@
 package com.doyouclub.backend.global.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext
 
 @EnableReactiveMongoAuditing
 @Configuration
-class MongoConfiguration
+class MongoConfiguration {
+    @Bean
+    fun reactiveMappingMongoConverter(mongoMappingContext: MongoMappingContext): MappingMongoConverter =
+        MappingMongoConverter(
+            ReactiveMongoTemplate.NO_OP_REF_RESOLVER,
+            mongoMappingContext
+        ).apply { setTypeMapper(DefaultMongoTypeMapper(null)) }
+}
