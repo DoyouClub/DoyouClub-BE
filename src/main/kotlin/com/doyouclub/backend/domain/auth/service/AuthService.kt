1234edaf0767a142d4b921e7779ef574fb418649
earlgrey02 <san06036@naver.com>
2024-09-28 21:04:04 +0900

    Feat: AuthService signUp() 구현

--- src/main/kotlin/com/doyouclub/backend/domain/auth/service/AuthService.kt
+++ src/main/kotlin/com/doyouclub/backend/domain/auth/service/AuthService.kt
@@ -2,18 +2,23 @@ package com.doyouclub.backend.domain.auth.service
 
 import com.doyouclub.backend.domain.auth.dto.request.RefreshRequest
 import com.doyouclub.backend.domain.auth.dto.request.SignInRequest
+import com.doyouclub.backend.domain.auth.dto.request.SignUpRequest
 import com.doyouclub.backend.domain.auth.dto.response.RefreshResponse
 import com.doyouclub.backend.domain.auth.dto.response.SignInResponse
-import com.doyouclub.backend.domain.auth.exception.InvalidAccessException
-import com.doyouclub.backend.domain.auth.exception.InvalidRefreshTokenException
-import com.doyouclub.backend.domain.auth.exception.RefreshTokenNotFoundException
+import com.doyouclub.backend.domain.auth.dto.response.SignUpResponse
+import com.doyouclub.backend.domain.auth.exception.*
 import com.doyouclub.backend.domain.auth.model.RefreshToken
 import com.doyouclub.backend.domain.auth.model.SignUpToken
 import com.doyouclub.backend.domain.auth.repository.RefreshTokenRepository
 import com.doyouclub.backend.domain.auth.repository.SignUpTokenRepository
 import com.doyouclub.backend.domain.user.exception.UserNotFoundException
+import com.doyouclub.backend.domain.user.model.User
+import com.doyouclub.backend.domain.user.model.enum.Role
 import com.doyouclub.backend.domain.user.repository.UserRepository
 import com.doyouclub.backend.global.jwt.core.JwtProvider
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import org.springframework.stereotype.Service
 
 @Service
@@ -77,5 +82,33 @@ class AuthService(
 
     suspend fun logout(id: String) {
         refreshTokenRepository.deleteByKey(id)
+    }
+
+    suspend fun signUp(request: SignUpRequest): SignUpResponse = coroutineScope {
+        with(request) {
+            val getUserJob = launch { userRepository.findByEmail(email) ?: throw AccountAlreadyExistException() }
+            val getSignUpTokenDeferred =
+                async { signUpTokenRepository.findByKey(email) ?: throw SignUpTokenNotFoundException() }
+
+            getUserJob.join()
+            val signUpToken = getSignUpTokenDeferred.await()
+
+            if (signUpToken.content != token) throw InvalidSignUpTokenException()
+
+            val user = userRepository.save(
+                User(
+                    email = email,
+                    name = name,
+                    provider = signUpToken.provider,
+                    roles = setOf(Role.MEMBER)
+                )
+            )
+            val (accessToken, refreshToken) = jwtProvider.createTokens(user)
+
+            SignUpResponse(
+                accessToken = accessToken,
+                refreshToken = refreshToken
+            )
+        }
     }
 }
