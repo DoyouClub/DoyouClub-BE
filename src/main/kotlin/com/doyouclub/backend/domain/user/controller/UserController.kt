3ea952373cc6dbb2ef197a3b3ee1ef2e37402b43
earlgrey02 <san06036@naver.com>
2024-10-07 14:22:21 +0900

    Feat: UserController updateUserById() 구현

--- src/main/kotlin/com/doyouclub/backend/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/doyouclub/backend/domain/user/controller/UserController.kt
@@ -1,12 +1,11 @@
 package com.doyouclub.backend.domain.user.controller
 
+import com.doyouclub.backend.domain.user.dto.request.UpdateUserByIdRequest
 import com.doyouclub.backend.domain.user.dto.response.UserResponse
 import com.doyouclub.backend.domain.user.service.UserService
 import com.doyouclub.backend.global.jwt.security.JwtAuthentication
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/user")
 @RestController
@@ -19,4 +18,13 @@ class UserController(
         authentication: JwtAuthentication
     ): UserResponse =
         userService.getUserById(authentication.id)
+
+    @PutMapping("/{id}")
+    suspend fun updateUserById(
+        @PathVariable
+        id: String,
+        @RequestBody
+        request: UpdateUserByIdRequest
+    ): UserResponse =
+        userService.updateUserById(id, request)
 }
