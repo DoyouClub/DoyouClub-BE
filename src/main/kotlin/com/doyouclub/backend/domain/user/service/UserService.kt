60d6262da907941ce1774b6dc2de818569812292
earlgrey02 <san06036@naver.com>
2024-10-07 14:21:21 +0900

    Feat: UserService updateUserById() 구현

--- src/main/kotlin/com/doyouclub/backend/domain/user/service/UserService.kt
+++ src/main/kotlin/com/doyouclub/backend/domain/user/service/UserService.kt
@@ -1,5 +1,6 @@
 package com.doyouclub.backend.domain.user.service
 
+import com.doyouclub.backend.domain.user.dto.request.UpdateUserByIdRequest
 import com.doyouclub.backend.domain.user.dto.response.UserResponse
 import com.doyouclub.backend.domain.user.exception.UserNotFoundException
 import com.doyouclub.backend.domain.user.repository.UserRepository
@@ -13,4 +14,10 @@ class UserService(
         userRepository.findById(id)
             ?.let { UserResponse(it) }
             ?: throw UserNotFoundException()
+
+    suspend fun updateUserById(id: String, request: UpdateUserByIdRequest): UserResponse {
+        val user = userRepository.findById(id) ?: throw UserNotFoundException()
+
+        return UserResponse(userRepository.save(request.toDomain(user)))
+    }
 }
